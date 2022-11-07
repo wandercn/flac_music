caca6a26955e76fae98b1028370540bd4427ff2e
lishenmiao <wander@ffactory.org>
2022-11-07 11:18:33 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ use std::thread::{sleep, spawn};
 fn main() {
     let win = WindowDesc::new(ui_builder)
         .menu(make_menu())
-        .title("Flac Music v0.10")
+        .title("Flac Music v0.2.0")
         .window_size((1200., 600.))
         .show_titlebar(true);
 
